5f5619c7f115ae14eab5a0c79776f99ea91b234b
FaramarzAf <faramarzafzali@gmail.com>
2020-03-16 12:27:28 +0330

    ready for v1.0.3

--- app/src/main/java/com/google/testairplaneencyclopedia/others/ContactUsActivity.java
+++ app/src/main/java/com/google/testairplaneencyclopedia/others/ContactUsActivity.java
@@ -1,27 +1,12 @@
 package com.google.testairplaneencyclopedia.others;
 
-import android.content.Intent;
-import android.graphics.drawable.Animatable;
-import android.net.Uri;
-import android.support.constraint.solver.widgets.Animator;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.testairplaneencyclopedia.R;
 
-import javax.xml.datatype.Duration;
-
 public class ContactUsActivity extends AppCompatActivity {
-
 
 
     @Override
